16c671fb13cabb80b3d4cc1367a98ab4f41c4780
wooseungw <woow070840@gmail.com>
2024-02-15 22:19:12 +0900

    122

--- train.py
+++ train.py
@@ -5,25 +5,31 @@ import torch.nn.functional as F
 import lightning as L
 from transformers import GPT2LMHeadModel
 model = GPT2LMHeadModel.from_pretrained('skt/kogpt2-base-v2')
+import torch
+from transformers import BertTokenizer, BertForSequenceClassification, AdamW
+from pytorch_lightning import LightningModule, Trainer
 
 
-class BaseModel(L.LightningModule):
+class TextClassificationModel(LightningModule):
     def __init__(self, model):
         super().__init__()
+        self.model = model
         
+    def forward(self, input_ids, attention_mask, labels=None):
+        output = self.model(input_ids, attention_mask=attention_mask, labels=labels)
+        return output
 
     def training_step(self, batch, batch_idx):
-        # training_step defines the train loop.
-        
-        
-        
-        
-        loss = F.mse_loss(x_hat, x)
+        input_ids, attention_mask, labels = batch
+        output = self(input_ids, attention_mask, labels)
+        loss = output.loss
         return loss
 
     def configure_optimizers(self):
-        optimizer = torch.optim.AdamW(self.parameters(), lr=1e-3)
-        return optimizer
-    
-if __name__ == '__main__':
-    print("hi")
+        return AdamW(self.parameters(), lr=2e-5)
+
+    def train_dataloader(self):
+        # 여기서는 DataLoader를 반환해야 합니다. 예를 들어:
+        # dataset = SomeDataset(...)
+        # return DataLoader(dataset, batch_size=32, shuffle=True)
+        pass
